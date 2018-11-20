7aebdef90db5ded83f2069cf37f22f80d29b11cd
mattwuu <shawmin.woo@gmail.com>
2018-11-21 00:40:34 +0800

    模块动态绑定网站域名

--- routes/web.php
+++ routes/web.php
@@ -13,42 +13,8 @@
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return app()->build(\Modules\Admin\Http\Controllers\HomeController::class)->index();
 });
-
-//Route::get('/phpinfo', function () {
-//    return dd(phpinfo());
-//});
-
-//Route::get('hello', function (){
-//    return 'This is a request from get method.';
-//});
-
-//Route::match(['get', 'post'], 'foo', function () {
-//    return 'this is a request from get or post methods.';
-//});
-//
-//Route::any('bar', function () {
-//    return '<form method="post" action="/foo">' . csrf_field() . '<button type="submit">提交</button></form>';
-//});
-//
-//Route::view('hello', 'hello', ['title' => 'Laravel', 'content' => '您好，欢迎来到Laravel的世界！']);
-
-/*Route::get('user/{id}/name/{name?}', function ($id, $name = 'JamesBond') {
-    return 'USER_ID : ' . $id . ' -- USER_NAME : ' . $name;
-});*/
-
-//Route::get('user/{id}/{name}', function ($id, $name) {
-//    return 'user id => ' . $id . ' --- ' . 'user_name => ' . $name;
-//})->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
-//
-//Route::get('user/{id}', 'UserController@show');
-//
-//Route::get('biz', function (){
-//
-//});
-
-
 
 Auth::routes();
 
